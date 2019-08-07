2f0d5a1a4df0c0e4990bec55fea5de5966bd42e0
mdw <mightbemike@pm.me>
2019-08-07 00:48:32 -0400

    cleaned up dns_query results, added optional nameserver specification

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-short python scripts using scapy
+### short python scripts using scapy:
+
+#### dns_query.py - show DNS lookup info for domains: A and NS recs
+* $ sudo ./dns_query.py \<domain>
+* $ sudo ./dns_query.py \<domain> A
+* $ sudo ./dns_query.py \<domain> A \<nameserver>
+* $ sudo ./dns_query.py \<domain> NS
+
+#### sort_packets.py - for each packet in capture if IP protocol, identify:
+* layer 2 source and destination MAC address
+* layer 3 source and destination IP address
+* transport layer protocol type, ports, TCP flags (TCP, UDP, ICMP only)
+
+usage:<br>
+$ ./sort_packets.py \<file.pcap>
 
--- dns_query.py
+++ dns_query.py
@@ -1,62 +1,55 @@
-#!/usr/bin/python3	
+#!/usr/bin/env python3	
 import sys, re
-
-NAMESERVER = '8.8.4.4'
-
-#from scapy.all import *
 from scapy.all import DNS, DNSQR, IP, UDP, sr1, conf, L3RawSocket
 
-# check for missing arg, valid domain name, => root perms
+NAMESERVER = sys.argv[3] if len(sys.argv) > 3 else '1.1.1.1'
+
+# check for missing arg, valid domain name
 if len(sys.argv) <= 1:
 	use1 = "Usage: sudo python3 dns_query.py <domain_name>\n"
-	use2 = "       sudo python3 dns_query.py <domain_name> [<rec_type(A|NS|MX|SOA|ALL)>]"
-	exit(use1 + use2)
+	use2 = "       sudo python3 dns_query.py <domain_name> [<rec_type(A|NS|ALL)>]\n"
+	use3 = "       sudo python3 dns_query.py <domain_name> A [<nameserver>]"
+	exit(use1 + use2 + use3)
+
+# test for valid domain name here 
 domain = sys.argv[1]
-
 validdomain = re.compile('^[a-z]\w*(?:\.\w{2,})+$')
-validarg2	= re.compile('A|ALL|MX|SOA|NS')
-
-# need to test for valid domain name here 
-# (or whatever is being tested for here, LOL)
 if validdomain.match(sys.argv[1]) == None:
 	exit("Sorry, \"{}\" is not a valid domain name".format(domain))
 
 # test for 2nd arg, what type result
+validarg2 = re.compile('A|ALL|MX|SOA|NS')
 if len(sys.argv) > 2 and validarg2.match(sys.argv[2])==None:
-	info = "is not a valid option. Please choose A, NS, or ALL"
+	info = "is not a valid option. Please choose A, NS or ALL"
 	exit("Sorry, \"{}\" {}".format(sys.argv[2], info))
     
-# configure raw sockets
+# finally ready to start DNS query
 conf.L3socket
 conf.L3socket=L3RawSocket
 
-# finally ready to start DNS query
 i = IP(dst=NAMESERVER)
 u = UDP(dport=53)
+resp = sr1(i/u/DNS(rd=1, qd=DNSQR(qname=domain, qtype='A')), verbose=0)
 
 # what result did the user ask for?
 if len(sys.argv) > 2:
-	rec = sys.argv[2]
 
-	if rec == 'A' or rec == 'NS':
-		resp = sr1(i/u/DNS(rd=1, qd=DNSQR(qname=domain, qtype='A')))
-	else:
-		resp = sr1(i/u/DNS(rd=1, qd=DNSQR(qname=domain)))
+	if sys.argv[2] == 'A':
+		print("Nameserver: {}".format(NAMESERVER))
+		print("Name:       {}".format(domain))
+		print("Address:    {}".format(resp.an.rdata))
 
-	# print results
-	if rec == 'A':
-		print("DNS lookup results for {}:\n".format(domain))
-		print(resp.an.display())		# print full A rec
-	elif rec == 'NS':
-		# To Do: check resp.nscount then loop thru and print NS entries
-		print("{} Nameservers found for {}:\n".format(resp.nscount, domain))
-		print(resp.ns.display())
-	elif rec == 'ALL' or rec == 'all':
+	elif sys.argv[2] == 'NS':
+		print("{} Nameservers found for {}:".format(resp.nscount, domain))
+		for n in range(0,resp.nscount):
+			print(resp.ns[n].rdata.decode('utf-8'))
+
+	elif sys.argv[2] == 'ALL':
 		#print(resp.getlayer(DNS).display())
 		print(resp.display())
 else: 	
-	resp = sr1(i/u/DNS(rd=1, qd=DNSQR(qname=domain)))
+	resp = sr1(i/u/DNS(rd=1, qd=DNSQR(qname=domain)), verbose=0)
 	#print(resp.summary())	# pretty response
-	print("IP address for {}: {}".format(domain, resp.an.rdata))
-
+	print("Name:    {}".format(domain))
+	print("Address: {}".format(resp.an.rdata))
 
